149ad112898a6eec601bfced7d1ccb5927f2b8b9
mnap <learn@10g.pl>
2018-05-18 22:20:06 +0200

    Add provider; adjust project structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store';
 
 render (
-    <h1>Initiate Project</h1>,
+    <Provider store={store}>
+        <h1>Initiate Project</h1>
+    </Provider>,
     document.getElementById('root')
 );
