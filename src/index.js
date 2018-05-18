475ec9d300116e1b06196e21941a20523aca1d21
mnap <learn@10g.pl>
2018-05-18 22:39:26 +0200

    Install devtools, add import/export and test

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './store';
+import DevTools from './devtools';
+import * as actions from './actions/actions-countries';
 
 render (
     <Provider store={store}>
-        <h1>Initiate Project</h1>
+        <div>
+            <h1>Initiate Project</h1>
+            <DevTools />
+        </div>
     </Provider>,
     document.getElementById('root')
 );
+store.dispatch(actions.getCountries());
