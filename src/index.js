eb56250204f3d72462cba2ae6185e704abcfddfd
mnap <learn@10g.pl>
2018-05-19 17:05:04 +0200

    Move routes to separate file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Router, Route, hashHistory} from 'react-router';
+import {Router, hashHistory} from 'react-router';
 import {Provider} from 'react-redux';
 import store from './store';
+import routes from './routes';
 import DevTools from './devtools';
 import * as actions from './actions/actions-countries';
-import Navigation from './components/navigation.component';
 
 render (
     <Provider store={store}>
-        <Router history={hashHistory}>
-            <Route path='/' component={Navigation}>
-            </Route>
+        <div>
+            <Router history={hashHistory} routes={routes}/>
             <DevTools />
-        </Router>
+        </div>
     </Provider>,
     document.getElementById('root')
 );
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Route} from 'react-router';
+import Navigation from './components/navigation.component';
+
+export default (
+    <Route path='/' component={Navigation}>
+    </Route>
+);
