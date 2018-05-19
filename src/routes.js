9aad8012671c4371d0aca86b6bf1130f236900a7
mnap <learn@10g.pl>
2018-05-19 18:37:02 +0200

    Add routes for components

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import {Route} from 'react-router';
+import {Route, IndexRoute} from 'react-router';
 import Navigation from './components/navigation.component';
+import Home from './components/home.component';
+import Contact from './components/contact.component';
+import NotFound from './components/not-found.component';
 
 export default (
     <Route path='/' component={Navigation}>
+        <IndexRoute component={Home}/>
+        <Route path='contact' component={Contact}/>
+        <Route path='*' component={NotFound}/>
     </Route>
 );
