6ad6670638b6fd5779cad28376785dda4f328688
mnap <learn@10g.pl>
2018-05-20 23:24:39 +0200

    Fix navbar responsiveness

--- src/components/navigation.component.js
+++ src/components/navigation.component.js
@@ -4,37 +4,39 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const Navigation = props => (
     <div>
-        <nav className='navbar navbar-expand navbar-light bg-light'>
+        <nav className='navbar navbar-expand-sm navbar-light bg-light'>
             <div className='container-fluid'>
                 <div className='navbar-header'>
                     <Link className='navbar-brand' to='/'>Countries.js</Link>
                 </div>
-                <ul className='navbar-nav mr-auto'>
-                    <li className='nav-item p-1'>
-                        <Link
-                            className='nav-link'
-                            to='/countries'
-                        >
-                            Countries
-                        </Link>
-                    </li>
-                    <li className='nav-item p-1'>
-                        <Link
-                            className='nav-link'
-                            to='/continents'
-                        >
-                            Continents
-                        </Link>
-                    </li>
-                    <li className='nav-item p-1'>
-                        <Link
-                            className='nav-link'
-                            to='/contact'
-                        >
-                            Contact
-                        </Link>
-                    </li>
-                </ul>
+                <div className='navbar-collapse'>
+                    <ul className='navbar-nav mr-auto'>
+                        <li className='nav-item p-1'>
+                            <Link
+                                className='nav-link'
+                                to='/countries'
+                            >
+                                Countries
+                            </Link>
+                        </li>
+                        <li className='nav-item p-1'>
+                            <Link
+                                className='nav-link'
+                                to='/continents'
+                            >
+                                Continents
+                            </Link>
+                        </li>
+                        <li className='nav-item p-1'>
+                            <Link
+                                className='nav-link'
+                                to='/contact'
+                            >
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
         <div className='container-fluid py-5'>
