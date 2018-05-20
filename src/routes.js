d46689cc1f911564e16a39ef49f9be6fc8932846
mnap <learn@10g.pl>
2018-05-20 11:59:35 +0200

    Add get country reducer and route

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,16 @@ import Home from './components/home.component';
 import Contact from './components/contact.component';
 import NotFound from './components/not-found.component';
 import CountryFlagContainer from './containers/flag-container.component';
+import CountryDetailsContainer
+    from './containers/country-detail-container.component';
 
 export default (
     <Route path='/' component={Navigation}>
         <IndexRoute component={Home}/>
-        <Route path='countries' component={CountryFlagContainer}/>
+        <Route path='countries'>
+            <IndexRoute component={CountryFlagContainer}/>
+            <Route path='country/:id' component={CountryDetailsContainer}/>
+        </Route>
         <Route path='contact' component={Contact}/>
         <Route path='*' component={NotFound}/>
     </Route>
